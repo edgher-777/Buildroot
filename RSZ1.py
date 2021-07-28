3d3b8b7a27ab5fdc030ef416b2365ceecb1791bb
Edgar Hernandez <eligio.hernandez777@gmail.com>
2021-07-28 15:28:33 -0500

    Semi

--- RSZ1.py
+++ RSZ1.py
@@ -2,14 +2,16 @@ import os
 
 print("Checking partition space")
 envVar=os.popen("fw_printenv partitionData").read()
+print(envVar)
 
 while envVar != "resized":
     envVar=os.popen("fw_printenv partitionData").read()
     try:
         partitionSize=int(os.popen("df -h --out=size /dev/mmcblk0p6").read())
+        print(partitionSize)
     except:
         partitionSize=0
-    if partitionSize <= 7000000:
+    if partitionSize <= 7000000 and partitionSize > 1024 :
         print("Resizing Partition")
         resizePartition=os.popen("resize2fs /dev/mmcblk0p6")
         setEnvVar=os.popen("fw_setenv partitionData resized").read()
