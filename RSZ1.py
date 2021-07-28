52538f2d84551abe2d0acb945e5c6200f6b3648b
Edgar Hernandez <ehernandez@hki.com>
2021-07-28 12:44:07 -0500

    Intermedio

--- RSZ1.py
+++ RSZ1.py
@@ -9,9 +9,10 @@ while envVar != "resized":
     envVar=os.popen("fw_printenv partitionData").read()
     partitionSize=int(os.popen("df -h --out=size /dev/mmcblk0p6").read())
     if partitionSize < 7000000:
+        logger.debug("Resizing Partition")
         resizePartition=os.popen("resize2fs /dev/mmcblk0p6")
         setEnvVar=os.popen("fw_setenv partitionData resized").read()
     else:
         setEnvVar=os.popen("fw_setenv partitionData resized").read()
-
-os.popen("systemctl disable resizeOnce")
+logger.debug("Disabling rezise service")
+os.popen("systemctl disable resizeOnce").read()
