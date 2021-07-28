04791975bcd4cedf362d317dd299102236dc4357
Edgar Hernandez <eligio.hernandez777@gmail.com>
2021-07-28 15:22:22 -0500

    Intra

--- RSZ1.py
+++ RSZ1.py
@@ -9,16 +9,16 @@ while envVar != "resized":
         partitionSize=int(os.popen("df -h --out=size /dev/mmcblk0p6").read())
     except:
         partitionSize=0
-    if partitionSize < 7000000:
+    if partitionSize <= 7000000:
         print("Resizing Partition")
         resizePartition=os.popen("resize2fs /dev/mmcblk0p6")
         setEnvVar=os.popen("fw_setenv partitionData resized").read()
-    elif partitionSize=0:
+    elif partitionSize==0:
         print("Partitionunavailable")
         setEnvVar=os.popen("Fw_setenv partitionData unavailable")
     else:
         print("Partition rezised")
         setEnvVar=os.popen("fw_setenv partitionData resized").read()
-        
+
 print("Disabling rezise service")
 os.popen("systemctl disable resizeOnce").read()
