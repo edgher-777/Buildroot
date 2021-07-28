559a49bc18d218387061bfe32ba8f8244c053e1c
Edgar Hernandez <eligio.hernandez777@gmail.com>
2021-07-28 15:21:21 -0500

    Inter

--- RSZ1.py
+++ RSZ1.py
@@ -1,17 +1,24 @@
 import os
 
 print("Checking partition space")
-envVar=os.popen("fw_printenv partitionData").strip().read()
+envVar=os.popen("fw_printenv partitionData").read()
 
 while envVar != "resized":
     envVar=os.popen("fw_printenv partitionData").read()
-    partitionSize=int(os.popen("df -h --out=size /dev/mmcblk0p6").read())
+    try:
+        partitionSize=int(os.popen("df -h --out=size /dev/mmcblk0p6").read())
+    except:
+        partitionSize=0
     if partitionSize < 7000000:
-        logger.debug("Resizing Partition")
+        print("Resizing Partition")
         resizePartition=os.popen("resize2fs /dev/mmcblk0p6")
         setEnvVar=os.popen("fw_setenv partitionData resized").read()
+    elif partitionSize=0:
+        print("Partitionunavailable")
+        setEnvVar=os.popen("Fw_setenv partitionData unavailable")
     else:
-        logger.debug("Partition rezised")
+        print("Partition rezised")
         setEnvVar=os.popen("fw_setenv partitionData resized").read()
-logger.debug("Disabling rezise service")
+        
+print("Disabling rezise service")
 os.popen("systemctl disable resizeOnce").read()
