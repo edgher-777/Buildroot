b9b9c700f5484fc243efd923ba2f74c541cfb98c
Zept777 <80482986+edgher-777@users.noreply.github.com>
2021-07-28 17:02:50 -0500

    Update RSZ1.py
    Info scrapping version

--- RSZ1.py
+++ RSZ1.py
@@ -7,17 +7,19 @@ print(envVar)
 while envVar != "resized":
     envVar=os.popen("fw_printenv partitionData").read()
     try:
-        partitionSize=int(os.popen("df -h --out=size /dev/mmcblk0p6").read())
+        s=os.popen("df /dev/mmcblk0p6 | grep /dev/mmcblk0p6").read()
+        l=s.split()
+        partitionSize=int(l[1])
         print(partitionSize)
     except:
         partitionSize=0
-    if partitionSize <= 7000000 and partitionSize > 1024 :
+    if partitionSize <= 800000 and partitionSize > 1024 :
         print("Resizing Partition")
         resizePartition=os.popen("resize2fs /dev/mmcblk0p6")
         setEnvVar=os.popen("fw_setenv partitionData resized").read()
     elif partitionSize==0:
-        print("Partitionunavailable")
-        setEnvVar=os.popen("Fw_setenv partitionData unavailable")
+        print("Partition unavailable")
+        setEnvVar=os.popen("fw_setenv partitionData unavailable")
     else:
         print("Partition rezised")
         setEnvVar=os.popen("fw_setenv partitionData resized").read()
