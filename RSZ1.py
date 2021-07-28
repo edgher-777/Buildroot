2d22be17badd8b3cafda2c4e0dbdaa9febbf0663
Edgar Hernandez <ehernandez@hki.com>
2021-07-28 12:34:48 -0500

    Intermedio

--- RSZ1.py
+++ RSZ1.py
@@ -2,12 +2,12 @@ import os
 import logging
 
 logger = logging.getLogger("DEBUG")
-debug_logger.debug("Checking partition space")
-envVar=os.popen("fw_printenv partitionData").read()
+logger.debug("Checking partition space")
+envVar=os.popen("fw_printenv partitionData").strip().read()
 
-while envVar != "resized"
-envVar=os.popen("fw_printenv partitionData").read()
-partitionSize=int(os.popen("df -h --out=size /dev/mmcblk0p6").read())
+while envVar != "resized":
+    envVar=os.popen("fw_printenv partitionData").read()
+    partitionSize=int(os.popen("df -h --out=size /dev/mmcblk0p6").read())
     if partitionSize < 7000000:
         resizePartition=os.popen("resize2fs /dev/mmcblk0p6")
         setEnvVar=os.popen("fw_setenv partitionData resized").read()
