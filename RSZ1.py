441a771050e15c8b3778782e4b0fb2883473cb6a
Zept777 <80482986+edgher-777@users.noreply.github.com>
2021-07-29 08:22:21 -0500

    Update RSZ1.py

--- RSZ1.py
+++ RSZ1.py
@@ -1,11 +1,10 @@
 import os
 
 print("Checking partition space")
-envVar=os.popen("fw_printenv partitionData{-.ñ+.,,{ñ+:;[Ñ*¡?=)(").read()
+envVar=os.popen("fw_printenv partitionData").read()
 print(envVar)
 
 while envVar != "partitionData=resized\n":
-    envVar=os.popen("fw_printenv partitionData").read()
     try:
         s=os.popen("df /dev/mmcblk0p6 | grep /dev/mmcblk0p6").read()
         l=s.split()
