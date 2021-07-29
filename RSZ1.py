4caefb405355c95c77b571b7673f37045cae4ee2
Zept777 <80482986+edgher-777@users.noreply.github.com>
2021-07-29 15:37:49 -0500

    Update RSZ1.py

--- RSZ1.py
+++ RSZ1.py
@@ -1,28 +1,19 @@
 import os
 
-print("Checking partition space")
 envVar=os.popen("fw_printenv partitionData").read()
-print(envVar)
 
 while not envVar == "partitionData=resized\n":
     try:
         s=os.popen("df /dev/mmcblk0p6 | grep /dev/mmcblk0p6").read()
         l=s.split()
         partitionSize=int(l[1])
-        print(partitionSize)
     except:
         partitionSize=0
     if partitionSize <= 800000 and partitionSize > 1024 :
-        print("Resizing Partition")
         resizePartition=os.popen("resize2fs /dev/mmcblk0p6")
-        setEnvVar=os.popen("fw_setenv partitionData resized").read()
-        print("Disabling resize service")
-        #os.popen("systemctl stop resizeOnce").read()
+        os.popen("fw_setenv partitionData resized").read()
     elif partitionSize==0:
-        print("Partition unavailable")
-        setEnvVar=os.popen("fw_setenv partitionData unavailable")
+        os.popen("fw_setenv partitionData unavailable")
     else:
-        print("Partition ready")
-        setEnvVar=os.popen("fw_setenv partitionData resized").read()
-        #os.popen("systemctl stop resizeOnce").read()
+        os.popen("fw_setenv partitionData resized").read()
     envVar=os.popen("fw_printenv partitionData").read()
